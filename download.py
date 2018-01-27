bccebfcb848116fdf89132dd09c2c538666821ca
Collector <gilad.david95@gmail.com>
2018-01-27 23:16:41 +0200

    oops lol x2

--- download.py
+++ download.py
@@ -27,10 +27,7 @@ def download_beatmaps(interval):
     other_beatmap_numbers -= my_beatmap_numbers
 
     # delete the my_beatmap.txt file cuz you don't need it anymore
-    try:
-        os.remove('my_beatmaps.txt')
-    except OSError:
-        print('Something went wrong, please restart the program')
+    os.remove('my_beatmaps.txt')
 
     # create download links for each of the numbers left on his list
     beatmap_link_list = ["https://osu.ppy.sh/d/" + str(beatmap_number) for beatmap_number in other_beatmap_numbers]
