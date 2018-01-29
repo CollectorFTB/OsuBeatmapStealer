fb1553f9318c3284c00e718bc91b7c197279649f
Collector <gilad.david95@gmail.com>
2018-01-29 15:54:25 +0200

    fixed progress bar, pep8
    thread doesnt always return the beatmap name and as a result doesnt print the beatmap name after each download

--- download.py
+++ download.py
@@ -2,44 +2,42 @@ import os
 import re
 import tempfile
 import zipfile
-from os import remove
-from os.path import join
-from time import sleep
-from webbrowser import open_new_tab
-
 import concurrent.futures
 import requests
 from requests import Session
 from tqdm import tqdm
 
-import config
-from path_helper import get_path
+
+def change_format(line):
+    return re.search("(\d+)", line).group(1)
 
 
-def download_beatmaps(my_beatmap_numbers,other_beatmaps_path,osu_dir):
-    # get path and save it for later
-    songs_dir = join(osu_dir,"Songs")
+def download_beatmaps(my_beatmap_numbers, other_beatmaps_path, songs_dir):
     # open up the file for writing
     with open(other_beatmaps_path, 'r') as other_beatmaps_file:
         # get other numbers
-        other_beatmap_numbers = {line.strip() for line in other_beatmaps_file}
+        other_beatmap_numbers = {change_format(line) for line in other_beatmaps_file}
+
     # remove your beatmaps from his list and eliminate duplicates
     other_beatmap_numbers -= my_beatmap_numbers
+
+    # starts session with osu
     if other_beatmap_numbers:
-        osu_session = Osu_Session(songs_dir) #TODO:switch to actual song library
+        osu_session = OsuSession(songs_dir)
         osu_session.download_beatmap_list(other_beatmap_numbers)
 
-class Osu_Session():
+
+class OsuSession:
     def __init__(self, songs_dir):
         self.osu = "https://osu.ppy.sh"
         self.songs_dir = songs_dir
         self.session = Session()
-        self._form = {"username":"dummyosu","password":"rEqUEsts12"}
+        self._form = {"username": "dummyosu", "password": "rEqUEsts12"}
         self._login()
 
     def _login(self):
-        self.session.get(self._endpoint("home")) #get XSRF-TOKEN to prove we're not a malicous phishing program
-        pst = self.session.post(self._endpoint("session"),data=self.form)
+        self.session.get(self._endpoint("home"))  # get XSRF-TOKEN to prove we're not a malicious phishing program
+        self.session.post(self._endpoint("session"), data=self.form)
 
     @property
     def form(self):
@@ -50,57 +48,55 @@ class Osu_Session():
         self._form['_token'] = self.session.cookies['XSRF-TOKEN']
 
     def _endpoint(self, *args):
-        ret= "/".join([self.osu] + list(args))
+        ret = "/".join([self.osu] + list(args))
         return ret
 
     @staticmethod
     def attached_file_name(response):
         header = response.headers['Content-Disposition']
-        #'attachment;filename="80 Ai Otsuka - Sakuranbo.osz";'
-        return re.search('filename="(.*).osz";',header).group(1)
-        #'80 Ai Otsuka - Sakuranbo'
+        # 'attachment;filename="80 Ai Otsuka - Sakuranbo.osz";'
+        return re.search('filename="(.*).osz";', header).group(1)
+        # '80 Ai Otsuka - Sakuranbo'
 
     @staticmethod
     def attached_file_length(response):
         return int(response.headers['Content-Length'])
 
-
     def download_beatmap(self, beatmap_number):
         try:
             download = self.session.get(self._endpoint(
-                "beatmapsets", beatmap_number, "download"),stream=True)
+                "beatmapsets", beatmap_number, "download"), stream=True)
             download.raise_for_status()
             with tempfile.TemporaryFile() as f:
-                # TODO:Adjust chunck size
-                chunck_size = 128
-                file_size = self.attached_file_length(download) 
+                # TODO: Adjust chunk size
+                chunk_size = 128
                 beatmap_name = self.attached_file_name(download)
-                for chunck in download.iter_content(chunk_size=chunck_size):
-                    f.write(chunck)
+                for chunk in download.iter_content(chunk_size=chunk_size):
+                    f.write(chunk)
                 self.extract_beatmap(f, beatmap_name)
+            return beatmap_name
         except requests.exceptions.HTTPError:
-            print("Beatmap {} does not exist".format(beatmap_number))
+            tqdm.write("Beatmap {} does not exist".format(beatmap_number))
         except KeyError:
-            print("Login expiried/failed")
+            tqdm.write("Login expired/failed")
             self._login()
             self.download_beatmap(beatmap_number)
         finally:
-            return beatmap_name
+            return ''
 
     def download_beatmap_list(self, beatmap_list):
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            future_to_url = {executor.submit(self.download_beatmap, beatmap): beatmap for beatmap in beatmap_list}
-            for thread in tqdm(concurrent.futures.as_completed(future_to_url),desc="Downloaded",unit='Beatmaps'):
-                print(thread.result())
+            threads = {executor.submit(self.download_beatmap, beatmap): beatmap for beatmap in beatmap_list}
+            for thread in tqdm(concurrent.futures.as_completed(threads), ncols=100,  desc="Downloaded", unit=' Beatmaps', total=len(beatmap_list)):
                 if thread.exception():
                     raise thread.exception()
 
     def extract_beatmap(self, beatmap_file, beatmap_name):
-        zipmap = zipfile.ZipFile(beatmap_file)
+        zip_map = zipfile.ZipFile(beatmap_file)
         beatmap_dir = os.path.join(self.songs_dir, beatmap_name)
         try:
             os.mkdir(beatmap_dir)
         except FileExistsError:
             print("beatmap already exists")
             raise
-        zipmap.extractall(path=beatmap_dir)
+        zip_map.extractall(path=beatmap_dir)
